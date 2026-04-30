76a63aeb8e12fd8c8ab55c482e9035aa7fd97a24
plevin22 <kevinpasztor10@gmail.com>
2026-04-30 21:03:05 +0200

    Cleaning up the main branch

--- src/main/java/com/mycompany/vizsgaremek/config/JWTNeeded.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.vizsgaremek.config;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import javax.ws.rs.NameBinding;
-
-@NameBinding
-@Retention(RetentionPolicy.RUNTIME)
-public @interface JWTNeeded {}
